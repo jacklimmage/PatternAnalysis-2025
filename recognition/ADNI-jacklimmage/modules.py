9af0e3307efe52fd393f08c718cf50cd27a5ffd3
Jack <133090850+jacklimmage@users.noreply.github.com>
2025-10-25 20:15:50 +1000

    init modules

---
+++ recognition/ADNI-jacklimmage/modules.py
@@ -0,0 +1,16 @@
+import torch
+import torch.nn as nn
+import torch.fft
+
+class GlobalFilter(nn.Module):
+    def __init__(self, dim, h=14, w=8):
+        super().__init__()
+        self.complex_weight = nn.Parameter(torch.randn(h, w, dim, 2, dtype=torch.float32) * 0.02)
+
+    def forward(self, x):
+        B, H, W, C = x.shape
+        x = torch.fft.rfft2(x, dim=(1, 2), norm='ortho')
+        weight = torch.view_as_complex(self.complex_weight)
+        x = x * weight
+        x = torch.fft.irfft2(x, s=(H, W), dim=(1, 2), norm='ortho')
+        return x
