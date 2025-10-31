cc9792a5d602288bd0e406f8801c3ba95dfb4978
Jack <133090850+jacklimmage@users.noreply.github.com>
2025-10-31 14:22:31 +1000

    changed transform slightly

--- recognition/ADNI-jacklimmage/dataset.py
+++ recognition/ADNI-jacklimmage/dataset.py
@@ -16,7 +16,7 @@ def load_data(batch_size=32, num_workers=4):
     ])
 
     test_transform = transforms.Compose([
-        # transforms.Resize(224),
+        transforms.Resize(256),
         transforms.CenterCrop(224),
         transforms.ToTensor(),
         transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])  # ImageNet stats
